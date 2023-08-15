c4428abd4e3cc1147f6d3303d2c61a78cbde0b08
Шипулин Павел <U2n_099@mail.ru>
2023-08-15 16:09:00 +0700

    по мелочи, зачатие requests.Session. На шару залететь не получилось

--- get_hrefs.py
+++ get_hrefs.py
@@ -1,42 +1,50 @@
 import requests
 from bs4 import BeautifulSoup as bs
+
 import pandas as pd
 import json
+
 import itertools
+import copy
 
 FILE_NAME = "rc/hh.ru-hrefs" + ".csv"
-URL_SITE = "https://novosibirsk.hh.ru/search/vacancy?text=&salary=&ored_clusters=true&area=4&page="  # page обязательно в конце
+URL_SITE = "https://novosibirsk.hh.ru/search/vacancy"  # ?text=&salary=&ored_clusters=true&area=4&page="  # page обязательно в конце
 '''
 text -- текст запроса
 salary -- минимальная зарплата
-ored_clusters -- хз
 area -- регион; 1 - Москва, 2 Питер, 3 Ебург, 4 Нск
 page -- номер отображаемой страницы конечного поиска
 '''
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'} # без этого r.status_code == 404 почему-то
+KEYS = {'area': 4, 'page': None, 'text': None, 'salary': None}
+REQUEST = 'python'
 
 
-def get_hrefs(request: str):
+def get_hrefs(query: str):
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'} # без этого r.status_code == 404 почему-то
-    
     vacancy_list = {'title': [], 'url': []}
+
+    keys = copy.deepcopy(KEYS)
+    keys['text'] = query
+    session = requests.Session()
+    session.headers['User-Agent'] = HEADERS['User-Agent']
+
     with open('rc/proxylist.json') as proxylist_file:
         
         proxy_list = json.load(proxylist_file)['data']
         num_of_proxy = itertools.cycle(range(len(proxy_list)))
-       
+
         for numpage in itertools.count():
-            
-            place_to_insert_request = URL_SITE.index('?text=') + 6
-            place_to_insert_numpage = URL_SITE.index('&page=') + 6
-            url = URL_SITE[:place_to_insert_request] + request + URL_SITE[place_to_insert_request:] + str(numpage)
-            
-            r = requests.get(url, headers=headers, proxies=proxy_list[next(num_of_proxy)])
-            if not r.status_code == 200:
+
+            keys['page'] = numpage
+            r = requests.get(URL_SITE, params=keys, headers=HEADERS, proxies=proxy_list[next(num_of_proxy)])
+            rs = session.get(URL_SITE, params=keys, headers=HEADERS, proxies=proxy_list[next(num_of_proxy)])
+
+            if r.status_code != 200 or rs.status_code != 200:
                 print(f"\nERROR: cannot create connection to {r.url}\nstatus code {r.status_code}\n")
                 return None
-            else:
-                print(f"parsing {r.url}\npage {numpage}")
+
+            print(f"parsing {r.url}\npage {numpage}")
 
             # находим все вакансии на текущей странице
             soup = bs(r.text, "html.parser")  # lxml
@@ -55,11 +63,5 @@ def get_hrefs(request: str):
 
 if __name__ == '__main__':
     
-    # rq = input().replace("+", ' ')
-    rq = 'python'
-
-    file = pd.DataFrame(data=get_hrefs(rq))
+    file = pd.DataFrame(data=get_hrefs(REQUEST))
     file.to_csv(FILE_NAME, sep=';', index=False)
-
-
-    
